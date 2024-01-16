adef86239265f198d18038bac6ae1859d3f93b52
solvent12521416 <alierhm1378@gmail.com>
2024-01-16 19:34:15 +0330

    initial commit to master

---
+++ SwiftUISampleApp/SwiftUISampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleAppApp.swift
+//  SwiftUISampleApp
+//
+//  Created by Ali on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
