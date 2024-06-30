73e40e63ec83d1c2ceb42702b58be91abd7a959a
solvent12521416 <alierhm1378@gmail.com>
2024-06-30 21:00:03 +0200

    add some thing for test

--- SwiftUISampleApp/ContentView.swift
+++ SwiftUISampleApp/ContentView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View  { 
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
+                .foregroundColor(.blue)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.blue)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
